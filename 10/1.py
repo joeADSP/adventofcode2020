0ba0d495d768e203a6d5092879c18454839cd29b
Joe Rowe <joe@adsp.ai>
2020-12-10 08:59:40 +0000

    part1 sort of one liner

---
+++ 10/1.py
@@ -0,0 +1,21 @@
+
+
+def load_data():
+    with open('data.txt', 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+
+def main():
+    data = [int(x) for x in load_data()]
+
+    data.sort()
+    data.insert(0, 0)
+    data.append(max(data) + 3)
+    counts = [len([x for x in [data[i + 1] - data[i] for i in range(len(data) - 1)] if x == y]) for y in (1, 3)]
+    print(counts[0]*counts[1])
+    return
+
+
+if __name__ == '__main__':
+    main()
