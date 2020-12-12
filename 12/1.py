5239bafd48128ceaf741063a29d3de579a9fa764
Joe Rowe <joe@adsp.ai>
2020-12-12 09:07:21 +0000

    day 12 first draft

---
+++ 12/1.py
@@ -0,0 +1,86 @@
+def load_data():
+    with open("data.txt", "r") as f:
+        data = f.read().splitlines()
+    return data
+
+
+def parse_instruction(instruction):
+    command = instruction[:1]
+    units = int(instruction[1:])
+    return command, units
+
+
+class Ferry:
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+        self._forward = self.move_east
+
+    def move_east(self, units):
+        self._x += units
+        return
+
+    def move_west(self, units):
+        self._x -= units
+        return
+
+    def move_north(self, units):
+        self._y += units
+        return
+
+    def move_south(self, units):
+        self._y -= units
+        return
+
+    def forward(self, units):
+        self._forward(units)
+        return
+
+    def rotate_clockwise(self, degrees):
+        self._rotate(degrees)
+        return
+
+    def rotate_anticlockwise(self, degrees):
+        self._rotate(360 - degrees)
+        return
+
+    def _rotate(self, degrees):
+        mapping = {self.move_east: self.move_south,
+                   self.move_south: self.move_west,
+                   self.move_west: self.move_north,
+                   self.move_north: self.move_east}
+        steps = degrees // 90
+        for i in range(steps):
+            self._forward = mapping[self._forward]
+        return
+
+    @property
+    def manhattan_distance(self):
+        return abs(self._x) + abs(self._y)
+
+
+def main():
+    data = load_data()
+    ferry = Ferry()
+    for instruction in data:
+        command, units = parse_instruction(instruction)
+        if command == "N":
+            ferry.move_north(units)
+        if command == "E":
+            ferry.move_east(units)
+        if command == "S":
+            ferry.move_south(units)
+        if command == "W":
+            ferry.move_west(units)
+        if command == "F":
+            ferry.forward(units)
+        if command == "R":
+            ferry.rotate_clockwise(units)
+        if command == "L":
+            ferry.rotate_anticlockwise(units)
+    print(ferry.manhattan_distance)
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ 12/2.py
@@ -0,0 +1,85 @@
+def load_data():
+    with open("data.txt", "r") as f:
+        data = f.read().splitlines()
+    return data
+
+
+def parse_instruction(instruction):
+    command = instruction[:1]
+    units = int(instruction[1:])
+    return command, units
+
+
+class Ferry:
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+        self._waypoint_x = 10
+        self._waypoint_y = 1
+        self._forward = self.move_east
+
+    def move_east(self, units):
+        self._waypoint_x += units
+        return
+
+    def move_west(self, units):
+        self._waypoint_x -= units
+        return
+
+    def move_north(self, units):
+        self._waypoint_y += units
+        return
+
+    def move_south(self, units):
+        self._waypoint_y -= units
+        return
+
+    def forward(self, units):
+        self._x += self._waypoint_x * units
+        self._y += self._waypoint_y * units
+        return
+
+    def rotate_clockwise(self, degrees):
+        self._rotate(degrees)
+        return
+
+    def rotate_anticlockwise(self, degrees):
+        self._rotate(360 - degrees)
+        return
+
+    def _rotate(self, degrees):
+        steps = degrees // 90
+        for i in range(steps):
+            self._waypoint_x, self._waypoint_y = self._waypoint_y, -self._waypoint_x
+        return
+
+    @property
+    def manhattan_distance(self):
+        return abs(self._x) + abs(self._y)
+
+
+def main():
+    data = load_data()
+    ferry = Ferry()
+    for instruction in data:
+        command, units = parse_instruction(instruction)
+        if command == "N":
+            ferry.move_north(units)
+        if command == "E":
+            ferry.move_east(units)
+        if command == "S":
+            ferry.move_south(units)
+        if command == "W":
+            ferry.move_west(units)
+        if command == "F":
+            ferry.forward(units)
+        if command == "R":
+            ferry.rotate_clockwise(units)
+        if command == "L":
+            ferry.rotate_anticlockwise(units)
+    print(ferry.manhattan_distance)
+    return
+
+
+if __name__ == "__main__":
+    main()
