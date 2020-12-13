0f46eed075ac8106547a02eb9640ee34487be565
Joe Rowe <joe@adsp.ai>
2020-12-13 11:01:24 +0000

    part 2 draft in python

---
+++ 13/2.py
@@ -0,0 +1,28 @@
+def load_data():
+    with open("data.txt", "r") as f:
+        data = f.read().splitlines()
+    return data
+
+
+def main():
+    data = load_data()
+
+    pairs = []
+    for i, time in enumerate(data[1].split(',')):
+        if time == 'x':
+            continue
+        pairs.append((int(time), int(i) + 1))
+
+    print(pairs)
+    n = 1
+    step = 1
+    for num, j in pairs:
+        while (n + j) % num != 0:
+            n += step
+        step *= num
+    print(n)
+    return
+
+
+if __name__ == "__main__":
+    main()
